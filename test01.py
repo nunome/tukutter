4b0f61bca6158aa8a926e871316d931fb07c791c
nunome <yuta12111989@yahoo.co.jp>
2017-11-28 10:08:07 +0000

    first commit

---
+++ test01.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def helloworld():
+    return 'hello world!'
+
+@app.route('/mypage')
+def mypagepage():
+    return 'nice to meet you!'
+        
